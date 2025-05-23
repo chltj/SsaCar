bb8828f0c6f6fe6ff10d531387584d9d181d60c7
seoyeon <chltj9810@naver.com>
2025-05-23 13:45:04 +0900

    메인페이지

---
+++ app/src/main/java/kr/ac/mjc/ssacar/CallHereActivity.java
@@ -0,0 +1,14 @@
+package kr.ac.mjc.ssacar;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CallHereActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_call_here);
+        }
+}
+
+
--- app/src/main/java/kr/ac/mjc/ssacar/MainActivity.java
+++ app/src/main/java/kr/ac/mjc/ssacar/MainActivity.java
@@ -1,24 +1,59 @@
 package kr.ac.mjc.ssacar;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView notificationIcon, mypageIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnCallHere, btnPickup, btnOneway, btnLongterm;
+
+        btnCallHere = findViewById(R.id.btn_call_here);
+        btnPickup = findViewById(R.id.btn_pickup);
+        btnOneway = findViewById(R.id.btn_oneway);
+        btnLongterm = findViewById(R.id.btn_longterm);
+
+        btnCallHere.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, CallHereActivity.class)));
+
+        btnPickup.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, PickupActivity.class)));
+
+        btnOneway.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, OnewayActivity.class)));
+
+        btnLongterm.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, LongtermActivity.class)));
+
+        notificationIcon = findViewById(R.id.notificationIcon);
+        mypageIcon = findViewById(R.id.mypageIcon);
+
+        notificationIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mypageIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyPageActivity.class);
+                startActivity(intent);
+            }
         });
     }
+
+
 }
