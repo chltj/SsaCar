aae82c196a597628d8ddb0c17b0195bcd6c16238
seoyeon <chltj9810@naver.com>
2025-05-23 13:45:04 +0900

    메인페이지

---
+++ app/src/main/java/kr/ac/mjc/ssacar/MyPageActivity.java
@@ -0,0 +1,13 @@
+// MyPageActivity.java
+package kr.ac.mjc.ssacar;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyPageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+    }
+}
