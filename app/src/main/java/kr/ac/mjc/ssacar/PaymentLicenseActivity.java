5d4d1ee50657cc8d8498a62dabed2c54224ee207
seoyeon <chltj9810@naver.com>
2025-05-23 21:09:01 +0900

    메인페이지 초안

---
+++ app/src/main/java/kr/ac/mjc/ssacar/PaymentLicenseActivity.java
@@ -0,0 +1,12 @@
+package kr.ac.mjc.ssacar;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PaymentLicenseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_license);
+    }
+}
